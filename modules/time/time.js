9868578ff6dd38033e447442949761c5a570b45b
daudi13 <davouma13@gmail.com>
2022-02-15 06:59:23 +0300

    Add Luxon

--- modules/time/time.js
+++ modules/time/time.js
@@ -1,17 +1,7 @@
+import { DateTime } from "../luxon/build/es6/luxon.js";
 import { timebox } from "../selector/selectors.js";
 
 export function time() {
-	const date = new Date();
-	const locale = navigator.language;
-	const options = {
-    month: 'long',
-    day: 'numeric',
-    year: 'numeric',
-    hour: 'numeric',
-    minute: 'numeric',
-    second: 'numeric',
-    hour12: false,
-	};
-	
+	const date = DateTime.local();
 	timebox.textContent = `${date.toLocaleTimeString(locale, options)}`;
 }
